17e5c26893745483b3e8e00afdd4062dfd61ee6f
ksullivan12 <101619616+ksullivan12@users.noreply.github.com>
2022-07-02 01:06:24 -0400

    first commit

---
+++ Exponent_Exercise2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Class_project
+{
+    class Program
+    
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our Exponent Program!");
+            bool runningoperation = true;
+            while (runningoperation == true)
+            {
+                runningoperation = false;
+            }
+            Console.WriteLine("Thank you for using our program! Good-Bye :)");
+        }
+    }
+}
